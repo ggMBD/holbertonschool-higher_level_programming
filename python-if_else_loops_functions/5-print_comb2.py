6a8f7109edb67e14f40ef2689553a3369edddfcd
maouia <maouiabend@gmail.com>
2023-09-27 20:42:03 +0100

    print with .format

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-for i in range(0, 99):
-    print(f"{i:02}", end=", ")
-print(i+1)
+for i in range(100):
+    if i == 99:
+        print("{}".format(i))
+    else:
+        print("{:02}".format(i), end=", ")
