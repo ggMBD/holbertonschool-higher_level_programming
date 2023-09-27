f50989551082857d943ffb312fd78d8971a04caa
maouia <maouiabend@gmail.com>
2023-09-27 22:44:42 +0100

    use variable

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def uppercase(str):
-    for i in range(len(str) - 1):
-        print("{}".format(chr(ord(str[i]) - 32)if ord(str[i])
-                          in range(97, 123) else str[i]), end='')
-    print("{}".format(chr(ord(str[i+1]) - 32) if ord(str[i+1])
-                      in range(97, 123) else str[i+1]), end="\n")
+    upp = ""
+    for char in str:
+        if 'a' <= char <= 'z':
+            upchar = chr(ord(char) - 32)
+        else:
+            upchar = char
+        upp += upchar
+    print(upp)
