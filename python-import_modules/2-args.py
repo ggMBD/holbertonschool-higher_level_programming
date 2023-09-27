29f5dc64848d0168e4b5b2ff5f62e61bba0ec8fb
maouia <maouiabend@gmail.com>
2023-09-28 00:44:17 +0100

    test

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
+import sys
 if __name__ == "__main__":
-    import sys
-    argc = len(sys.argv) - 1
-    if argc == 0:
+    if len(sys.argv) == 1:
         print("0 arguments.")
     else:
-        print("{} argument:".format(argc))
-        for i in range(1, argc + 1):
+        print("{} argument:".format(len(sys.argv) - 1))
+        for i in range(1, len(sys.argv)):
             print("{}: {}".format(i, sys.argv[i]))
