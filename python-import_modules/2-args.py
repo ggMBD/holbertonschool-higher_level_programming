879cf27a3acf13ba9a2e26288f57b551c603c82a
maouia <maouiabend@gmail.com>
2023-09-28 00:52:34 +0100

    fix if condition

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
-import sys
-if len(sys.argv) == 1:
-    print("0 arguments.")
-else:
-    print("{} argument:".format(len(sys.argv) - 1))
-    for i in range(1, len(sys.argv)):
-        print("{}: {}".format(i, sys.argv[i]))
+if __name__ == "__main__":
+    import sys
+    argc = len(sys.argv) - 1
+    if argc == 0:
+        print("0 arguments.")
+    elif argc == 1:
+        print("1 argument:")
+    else:
+        print("{} argument:".format(argc))
+    for i in range(argc):
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
