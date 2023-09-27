b9d11c9ee6dda218aad287cfbcc1dc7fca5891ca
maouia <maouiabend@gmail.com>
2023-09-27 20:39:15 +0100

    change code tyle

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-for i in range(100):
-    if i == 99:
-        print(f"{i}")
-    else:
-        print(f"{i:02}", end=", ")
+for i in range(0, 99):
+    print(f"{i:02}", end=", ")
+print(i+1)
