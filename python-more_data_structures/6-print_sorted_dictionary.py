7c79f8556e91d0a3337f365c35c9d12d93599ce4
maouia <maouiabend@gmail.com>
2023-10-02 22:39:34 +0100

    6-print_sorted_dictionary.py

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
+    if a_dictionary is not None:
 
-    sorted_dictionary = dict(sorted(a_dictionary.items()))
+        sorted_dictionary = dict(sorted(a_dictionary.items()))
 
-    for key, value in sorted_dictionary.items():
-        print("{}: {}".format(key, value))
+        for key, value in sorted_dictionary.items():
+            print("{}: {}".format(key, value))
