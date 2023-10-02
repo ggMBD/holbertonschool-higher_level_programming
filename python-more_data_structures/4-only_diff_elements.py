e881968e76713bcae6545fe95dc4ade4d766b64b
maouia <maouiabend@gmail.com>
2023-10-02 22:09:59 +0100

    add second loop

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -5,4 +5,8 @@ def only_diff_elements(set_1, set_2):
     for i in set_1:
         if (i not in set_2) and (i not in diff):
             diff.add(i)
+
+    for j in set_2:
+        if (j not in set_1) and (j not in diff):
+            diff.add(j)
     return diff
