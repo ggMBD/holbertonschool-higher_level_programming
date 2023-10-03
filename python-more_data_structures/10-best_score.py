4000f51844a99ecaeb967fd205b8ed61c8be4710
maouia <maouiabend@gmail.com>
2023-10-03 17:24:58 +0100

    best score

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    best = 0
+    name = ""
+    if a_dictionary == None:
+        return None
+    for key, value in a_dictionary.items():
+        if value > best:
+            best = value
+            name = key
+    return name
