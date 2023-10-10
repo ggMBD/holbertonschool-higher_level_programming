d8bc925aee1947a24c5df86fd70b9627fe5ff802
maouia <maouiabend@gmail.com>
2023-10-10 02:40:45 +0100

    fix

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -12,9 +12,9 @@ def add_integer(a, b=98):
     aise a TypeError exception with the message
     a must be an integer or b must be an integer
     """
-    if type(a) not in [int, float]:
+    if type(a) not in (int, float):
         raise TypeError("a must be an integer")
-    if type(b) not in [int, float]:
+    if type(b) not in (int, float):
         raise TypeError("b must be an integer")
     return int(a) + int(b)
 
