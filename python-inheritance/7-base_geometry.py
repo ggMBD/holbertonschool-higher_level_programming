a9d807677c961ae8a06f00acff5bd0d3655a39d0
maouia <maouiabend@gmail.com>
2023-10-19 00:47:36 +0100

    7-base_geometry.py

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" module base geometry """
+
+
+class BaseGeometry:
+    """ create class geometry with Public instance method: area """
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """ validates value """
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        elif value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
