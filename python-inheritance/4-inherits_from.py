c17ac21913944c781567aebb05af41b3d3012046
maouia <maouiabend@gmail.com>
2023-10-19 00:42:25 +0100

    fix

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
+""" module inherits from """
+
 
 def inherits_from(obj, a_class):
+    """ returns True if the object is an instance of\
+        a class that inherited from the specified class ;\
+        otherwise False"""
     return issubclass(type(obj), a_class)
