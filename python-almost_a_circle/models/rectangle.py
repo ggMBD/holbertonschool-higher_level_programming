3cbb170f504da8b749913e4ed163b3c4b85fa489
maouia <maouiabend@gmail.com>
2023-10-24 23:51:37 +0100

    add comment

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 from models.base import Base
+
 """
 module Rectangle
 """
@@ -7,10 +8,14 @@ module Rectangle
 
 class Rectangle(Base):
     """
-    create my first Rectangle with base parameter
+    A class representing a rectangle that inherits from the Base class.
     """
 
     def __init__(self, width, height, x=0, y=0, id=None):
+        """
+        Initialize a Rectangle object with the specified
+        width, height, x, y, and optional id.
+        """
         self.__width = width
         self.__height = height
         self.__x = x
@@ -19,32 +24,64 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        """
+        Get the width of the rectangle.
+
+        :return: The width of the rectangle.
+        """
         return self.__width
 
     @width.setter
     def width(self, value):
+        """
+        Set the width of the rectangle.
+
+        :param value: The new width for the rectangle.
+        """
         self.__width = value
 
     @property
     def height(self):
+        """
+        Get the height of the rectangle.
+
+        :return: The height of the rectangle.
+        """
         return self.__height
 
     @height.setter
     def height(self, value):
+        """
+        Set the height of the rectangle.
+
+        :param value: The new height for the rectangle.
+        """
         self.__height = value
 
     @property
     def x(self):
+        """
+        Get the x
+        """
         return self.__x
 
     @x.setter
     def x(self, value):
+        """
+        Set the x
+        """
         self.__x = value
 
     @property
     def y(self):
+        """
+        Get the y
+        """
         return self.__y
 
     @y.setter
     def y(self, value):
+        """
+        Set the y
+        """
         self.__y = value
