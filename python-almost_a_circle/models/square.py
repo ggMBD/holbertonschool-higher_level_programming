0be6d95b9f5097b19b9942acb9646ceb45942f42
maouia <maouiabend@gmail.com>
2023-10-27 20:59:58 +0100

    size

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -17,3 +17,26 @@ class Square(Rectangle):
         """ return the representation of a square """
         return "[Square] ({}) {}/{} - {}".\
             format(self.id, self.x, self.y, self.width)
+
+    @property
+    def size(self):
+        """
+        Get the size of the square.
+
+        :return: The sire of the square.
+        """
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """
+        Set the size of the square.
+
+        :param value: The new size for the square.
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.width = value
+        self.height = value
