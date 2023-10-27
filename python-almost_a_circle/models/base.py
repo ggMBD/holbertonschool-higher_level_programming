fcd30360e0b03af66c7672fa1cc0af3de13d4431
maouia <maouiabend@gmail.com>
2023-10-27 21:58:00 +0100

    docu

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -21,6 +21,9 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
+        """
+        returns the JSON string representation of list_dictionaries
+        """
         if list_dictionaries is None:
             return "[]"
         return json.dumps(list_dictionaries)
