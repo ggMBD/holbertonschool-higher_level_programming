25f0e7c4291abc0df79dfec1ee16e530509415c4
maouia <maouiabend@gmail.com>
2023-10-27 20:39:33 +0100

    fix

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -9,8 +9,10 @@ class Square(Rectangle):
     """
     class Square that inherits from Rectangle
     """
+
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
-        return f"[Square] (<{self.id}>) <{self.__x}>/<{self.__y}> - <{self.__width}>"
+        return "[Square] (<{}>) <{}>/<{}> - <{}>".\
+            format(self.id, self.x, self.y, self.width)
