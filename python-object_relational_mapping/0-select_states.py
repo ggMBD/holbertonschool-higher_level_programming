7c4e52dfe76d0641ebd1066c0e2b3d952e52a38e
maouia <maouiabend@gmail.com>
2023-11-28 23:53:47 +0100

    0. Get all states

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""
+Script that lists all states from the database hbtn_0e_0_usa.
+"""
+
+import mysql.connector as MySQLdb
+import sys
+
+if __name__ == "__main__":
+    """
+    Main function to connect to the MySQL server and fetch states data.
+    """
+
+    username, password, database = sys.argv[1:4]
+
+
+    # Connect to MySQL server
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
+    """Create a cursor object to interact with the database."""
+    cursor = db.cursor()
+
+    # Execute the SQL query
+    query = "SELECT * FROM states ORDER BY id"
+    cursor.execute(query)
+
+    # Fetch and display the results
+    results = cursor.fetchall()
+    for row in results:
+        print(row)
