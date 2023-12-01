56486c873ee3857e8db9be9fb52e92bd0ae4f04a
maouia <maouiabend@gmail.com>
2023-12-01 02:28:11 +0100

    fix session

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -7,7 +7,7 @@ script that lists all State objects from the database hbtn_0e_6_usa
 import sys
 from model_state import Base, State
 from sqlalchemy import create_engine
-from sqlalchemy.orm import session
+from sqlalchemy.orm import Session
 
 if __name__ == "__main__":
     engine = create_engine(
@@ -17,9 +17,7 @@ if __name__ == "__main__":
             sys.argv[3]),
         pool_pre_ping=True)
     Base.metadata.create_all(engine)
-    Session = session(engine)
-
-    session = Session()
+    session = Session(engine)
 
     states = session.query(State).order_by(State.id).all()
 
