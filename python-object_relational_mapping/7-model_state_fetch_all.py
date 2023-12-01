d8a572c9cb67fa71a7239ef1805394ea7ed69b40
maouia <maouiabend@gmail.com>
2023-12-01 02:39:29 +0100

    i need help

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -2,21 +2,18 @@
 """
 script that lists all State objects from the database hbtn_0e_6_usa
 """
-
-
-import sys
+from sys import argv
 from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
 
 if __name__ == "__main__":
     """create engine"""
-    engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-            sys.argv[1],
-            sys.argv[2],
-            sys.argv[3]),
-        pool_pre_ping=True)
+    path = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+            argv[1],
+            argv[2],
+            argv[3])
+    engine = create_engine(path)
     """ create session"""
     Base.metadata.create_all(engine)
     session = Session(engine)
